d53a36838750419a28e4b9921e4b55566849b68f
nozomu.k <nozomu.k@hacarus.com>
2023-03-03 16:43:01 +0900

    typo

--- install_scripts/dotsinstaller.sh
+++ install_scripts/dotsinstaller.sh
@@ -7,7 +7,7 @@ function main() {
     current_dir=$(dirname "${BASH_SOURCE[0]:-$0}")
     source $current_dir/lib/utilfuncs.sh
 
-    source $current_dir/lib/dotsinstaller/link-to-homedir.sh
+    source $current_dir/lib/link-to-homedir.sh
 
     print_info "#####################################################"
     print_info "$(basename "${BASH_SOURCE[0]:-$0}") link success!!!"

77b3097f34e8c8510c90a2dc68e13cb42e4cc7c7
nozomu.k <nozomu.k@hacarus.com>
2023-03-03 16:37:30 +0900

    implement main function

--- install.sh
+++ install.sh
@@ -2,15 +2,19 @@
 
 set -ue
 
-local current_dir
-current_dir=$(dirname "${BASH_SOURCE[0]:-$0}")
-source $current_dir/install_scripts/lib/utilfuncs.sh
+function main() {
+    local current_dir
+    current_dir=$(dirname "${BASH_SOURCE[0]:-$0}")
+    source $current_dir/install_scripts/lib/utilfuncs.sh
 
-"${current_dir}"/install_scripts/dotsinstaller.sh install
+    "${current_dir}"/install_scripts/dotsinstaller.sh install
 
-print_info ""
-print_info "#####################################################"
-print_info "$(basename "${BASH_SOURCE[0]:-$0}") install finish!!!"
-print_info "#####################################################"
-print_info ""
+    print_info ""
+    print_info "#####################################################"
+    print_info "$(basename "${BASH_SOURCE[0]:-$0}") install finish!!!"
+    print_info "#####################################################"
+    print_info ""
+    
 }
+
+main "$@"
--- install_scripts/dotsinstaller.sh
+++ install_scripts/dotsinstaller.sh
@@ -2,13 +2,17 @@
 
 set -ue
 
-local current_dir
-current_dir=$(dirname "${BASH_SOURCE[0]:-$0}")
-source $current_dir/lib/utilfuncs.sh
+function main() {
+    local current_dir
+    current_dir=$(dirname "${BASH_SOURCE[0]:-$0}")
+    source $current_dir/lib/utilfuncs.sh
 
-source $current_dir/lib/dotsinstaller/link-to-homedir.sh
+    source $current_dir/lib/dotsinstaller/link-to-homedir.sh
 
-print_info "#####################################################"
-print_info "$(basename "${BASH_SOURCE[0]:-$0}") link success!!!"
-print_info "#####################################################"
-print_info ""
+    print_info "#####################################################"
+    print_info "$(basename "${BASH_SOURCE[0]:-$0}") link success!!!"
+    print_info "#####################################################"
+    print_info ""
+}
+
+main "$@"

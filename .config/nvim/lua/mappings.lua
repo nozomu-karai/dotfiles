c37f46cb7db7ff06bdb116aff66e50786a54f100
n-karai <karai_nozomu@cyberagent.co.jp>
2025-01-14 11:58:53 +0900

    change: nvimのmappingの変更

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -10,6 +10,8 @@ vim.keymap.set("n", "sj", "<C-w>j")
 vim.keymap.set("n", "sk", "<C-w>k")
 vim.keymap.set("n", "sl", "<C-w>l")
 
+vim.keymap.set("i", "<C-e>", "<ESC>")
+
 vim.keymap.set("v", "y", "y`]")
 vim.keymap.set("v", "p", "p`]")
 vim.keymap.set("v", "<S-h>", "^")
